e522b67a9e2e89dc027eda15ead3af923e5e9db7
Dominik Fay <dominik.fay@gmx.de>
2016-05-03 18:48:27 +0200

    Improve readability by using from-imports

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from os.path import splitext
+from os.path import dirname, exists, isdir, join, splitext
 from uuid import uuid4
 
 import tornado.ioloop
@@ -16,7 +16,7 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 def new_session_id():
-    dirnames = [n for n in os.listdir(FILE_STORAGE_PATH) if os.path.isdir(os.path.join(FILE_STORAGE_PATH, n))]
+    dirnames = [n for n in os.listdir(FILE_STORAGE_PATH) if isdir(join(FILE_STORAGE_PATH, n))]
     session_id = None
     # the very unlikely case that the UUID has been generated before
     while session_id is None or session_id in dirnames:
@@ -26,14 +26,14 @@ def new_session_id():
 
 def new_json_file(session_id):
     dir_path = get_dataset_path(session_id)
-    file_path = os.path.join(dir_path, "meta.json")
-    if os.path.exists(file_path):
+    file_path = join(dir_path, "meta.json")
+    if exists(file_path):
         raise RuntimeError("JSON already exists: {}".format(file_path))
     return open(file_path, 'wb')
 
 
 def get_dataset_path(session_id):
-    return os.path.join(FILE_STORAGE_PATH, session_id)
+    return join(FILE_STORAGE_PATH, session_id)
 
 
 def new_data_files(imzml_filename, ibd_filename, session_id):
@@ -41,13 +41,13 @@ def new_data_files(imzml_filename, ibd_filename, session_id):
     filenames = os.listdir(dir_path)
     if any(fn.endswith('.imzml') or fn.endswith('.ibd') for fn in filenames):
         raise RuntimeError("Data already exist in directory {}".format(dir_path))
-    return open(os.path.join(dir_path, "{}.imzML".format(splitext(imzml_filename)[0])), 'wb'), open(
-        os.path.join(dir_path, "{}.ibd".format(splitext(ibd_filename)[0])), 'wb')
+    return open(join(dir_path, "{}.imzML".format(splitext(imzml_filename)[0])), 'wb'), open(
+        join(dir_path, "{}.ibd".format(splitext(ibd_filename)[0])), 'wb')
 
 
 def prepare_directory(session_id):
     dir_path = get_dataset_path(session_id)
-    if not os.path.isdir(dir_path):
+    if not isdir(dir_path):
         os.mkdir(dir_path)
 
 
@@ -81,13 +81,13 @@ def make_app():
         (r"/", MainHandler),
         (r"/submit", SubmitHandler),
     ],
-        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        static_path=join(dirname(__file__), "static"),
         static_url_prefix='/static/'
     )
 
 
 if __name__ == "__main__":
-    if not os.path.isdir(FILE_STORAGE_PATH):
+    if not isdir(FILE_STORAGE_PATH):
         os.mkdir(FILE_STORAGE_PATH)
     app = make_app()
     app.listen(9777)
