a3b714c5f713b310467dfce6702374514747d3b8
Dominik Fay <dominik.fay@gmx.de>
2016-05-17 15:46:49 +0200

    Fix missing dependencies; Ignore buffertools warning

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ var webpack = require('webpack');
 module.exports = {
     entry: './static/index.jsx',
     output: { path: 'static', filename: 'bundle.js' },
+    plugins: [
+        // https://github.com/mozilla-services/react-jsonschema-form#build-error-wrt-missing-buffertools-module
+        new webpack.IgnorePlugin(/^(buffertools)$/)
+    ],
     module: {
         loaders: [
             {
